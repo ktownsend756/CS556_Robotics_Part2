d9b90cd2ac3c5c695e65a50194bf86b9b2e83248
ktownsend756 <161529075+ktownsend756@users.noreply.github.com>
2025-10-01 19:06:56 -0700

    Update lab4.ino

--- lab4/lab4.ino
+++ lab4/lab4.ino
@@ -13,9 +13,9 @@ using namespace Pololu3piPlus32U4;
 #define gearRatio 75
 
 //Update kp and kd based on your testing
-#define minOutput -100
-#define maxOutput 100
-#define kp 0
+#define minOutput -300
+#define maxOutput 300
+#define kp 7
 #define base_speed 50
 
 Motors motors;
@@ -31,8 +31,10 @@ double wallDist;
 void setup() {
   Serial.begin(9600);
   servo.attach(5);
+  servo.write(180);
   delay(40);
   //Move Sonar to desired direction using Servo
+  
 }
 
 void loop() {
@@ -40,22 +42,27 @@ void loop() {
   wallDist = sonar.readDist();
 
   //UNCOMMENT AFTER IMPLEMENTING Pcontroller
-  //Pout = Pcontroller.update(wallDist, distFromWall); //uncomment if using Pcontroller 
+  double Pout = Pcontroller.update(wallDist, distFromWall); //uncomment if using Pcontroller 
 
-  //(LAB 4 - TASK 3.1) IMPLEMENT PCONTROLLER 
+  //(LAB 4 - TASK 3.1) IMPLEMENT PCONTROLLER (DONE)
   
   /*FIRST GO TO Pcontroller.h AND ADD PRIVATE VARIABLES NEEDED.
     THEN GO TO Pcontroller.cpp AND COMPLETE THE update FUNCTION.
     ONCE YOU IMPLEMENT update, UNCOMMENT CODE ABOVE TO USE CONTROLLER.*/
 
-  //(LAB 4 - TASK 3.2) PCONTROLLER WALL FOLLOWING
+  //(LAB 4 - TASK 3.2) PCONTROLLER WALL FOLLOWING (DONE)
+
+  //kp values (three values in 3 ranges) [0.1-.9] [1-10] [11 - 100]
 
   /*NOW THAT YOU HAVE IMPLEMENTED PCONTROLLER, TAKE THE OUTPUT FROM Pout
   AND SET THE MOTOR SPEEDS. CHANGE THE KP AND CLAMPING VALUES AT THE TOP
   TO TEST (B-D).
   Hint: Also use baseSpeed when setting motor speeds*/
+  int leftSpeed = base_speed + Pout;
+  int rightSpeed = base_speed - Pout;
+  motors.setSpeeds(leftSpeed, rightSpeed);
 
   //Also print outputs to serial monitor for testing purposes
-
+  Serial.println(wallDist);
 
 }
