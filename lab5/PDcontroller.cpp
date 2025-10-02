2556960776873277c4e6f3edf1e752eb3f67e9a3
ryanrenales <161529467+ryanrenales@users.noreply.github.com>
2025-10-01 23:32:51 -0700

    Update PDcontroller.cpp

--- lab5/PDcontroller.cpp
+++ lab5/PDcontroller.cpp
@@ -9,15 +9,14 @@ PDcontroller::PDcontroller(float kp, float kd, double minOutput, double maxOutpu
   _minOutput = minOutput;
   _maxOutput = maxOutput;
   _error = 0;
-  _prevError = 0;
-  _derivative = 0;
   _output = 0;
   _clampedOutput = 0;
-  _dampedOutput = 0;
-  
+  _prevError = 0;
+  _prevTime = 0;
+  _derivative = 0;
 }
 
-double PDcontroller::update(double value, double target_value, double lastError){
+double PDcontroller::update(double value, double target_value){
   //Controller math here
   /*Hints: To add damping (derivative), you must have something to
            keep track of time for the rate of change.
@@ -27,13 +26,26 @@ double PDcontroller::update(double value, double target_value, double lastError)
 
            Again, you need to return actuator controller value (_clampOut)
   */
-  //Store error by finding difference between goal distance from wall and actual distance
   _error = target_value - value;
+  long currentTime = millis();
+  double time = (currentTime - _prevTime) / 1000;
 
-  //Multiply the proportional gain and the error to determine how the robot should fix itself
-  _output = _kp * _error;
+  if(_prevTime == 0){
+    _output = _kp * _error;
+  } else{
+    _derivative = (_error - _prevError) / time;
+    _output = (_kp*_error) + (_kd*_derivative);
+  }
 
-  //Clamp the output so it does not exceed or drop below the min and max values
-  _clampedOutput = constrain(_output, _minOutput, _maxOutput);
+  if(_output > _maxOutput) 
+    _clampedOutput = _maxOutput;
+  else if (_output < _minOutput) 
+    _clampedOutput = _minOutput;
+  else 
+    _clampedOutput = _output;
 
+  _prevError = _error;
+  _prevTime = currentTime;
+  return _clampedOutput;
 }
+
