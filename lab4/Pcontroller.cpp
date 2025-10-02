b8921574ccde75e0afc83f2f7f87005ef529043d
ktownsend756 <161529075+ktownsend756@users.noreply.github.com>
2025-10-01 19:44:13 -0700

    Update Pcontroller.cpp

--- lab4/Pcontroller.cpp
+++ lab4/Pcontroller.cpp
@@ -4,9 +4,10 @@ using namespace Pololu3piPlus32U4;
 
 Pcontroller::Pcontroller(float kp, double minOutput, double maxOutput) {
   // initialize the private varaibles from Pcontroller.h here
-  _kp = kp;
-  _minOutput = minOutput;
-  _maxOutput = maxOutput;
+
+  _kp = kp; //Constant gain value to determine how strongly the robot corrects itself
+  _minOutput = minOutput; //Minimum clamping value (controller can't return a value below this)
+  _maxOutput = maxOutput; //Maximum clamping value (controller can't return a value above this)
   _error = 0;
   _output = 0;
   _clampedOutput = 0;
@@ -16,9 +17,15 @@ Pcontroller::Pcontroller(float kp, double minOutput, double maxOutput) {
 double Pcontroller::update(double value, double target_value){
   //Controller math here
   //Hint: Need to return actuator controller value (_clampOut)
+  
+  //Store error by finding difference between goal distance from wall and actual distance
   _error = target_value - value;
-  _output = _kp * _error;
-  _clampedOutput = constrain(_output, _minOutput, _maxOutput);
-  return _clampedOutput;
 
+  //Multiply the proportional gain and the error to determine how the robot should fix itself
+  _output = _kp * _error;
+
+  //Clamp the output so it does not exceed or drop below the min and max values
+  _clampedOutput = constrain(_output, _minOutput, _maxOutput);
+
+  return _clampedOutput;
 }
