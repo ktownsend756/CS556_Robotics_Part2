57297a55e5b40a32a191cd612fb4ff3aeb46c8f5
ryanrenales <161529467+ryanrenales@users.noreply.github.com>
2025-10-01 23:32:02 -0700

    Update PDcontroller.h

--- lab5/PDcontroller.h
+++ lab5/PDcontroller.h
@@ -6,22 +6,26 @@ using namespace Pololu3piPlus32U4;
 class PDcontroller{
   public:
     PDcontroller(float kp, float kd, double minOutput, double maxOutput);
-    double update(double value, double target_value, double lastError); //may need to update with additional variables passed in this function
+    double update(double value, double target_value); //may need to update with additional variables passed in this function
     
   private:
     //Add private variables here
     //Hint: You are adding 4 more variables to the
     //      ones from Pcontroller
+
     float _kp;
-    float _kd; //exclusive to PD
     double _minOutput;
     double _maxOutput;
     double _error;
-    double _prevError; //exclusive to PD
-    double _derivative; //exclusive to PD
     double _output;
     double _clampedOutput;
-    double _dampedOutput; //exclusive to PD
+
+    float _kd;
+    double _prevError;
+    double _prevTime;
+    double _derivative;
+
+	
 };
 
 #endif
