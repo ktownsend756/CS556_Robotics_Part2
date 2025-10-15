8c08f8abeecd2a71a91ef457c9491f08fe5e7dee
ryanrenales <161529467+ryanrenales@users.noreply.github.com>
2025-10-15 16:40:00 -0700

    Refactor lab6.ino for enhanced control and odometry
    Updated PID and PD controller parameters and integrated odometry functionality for improved robot navigation.

--- lab6/lab6.ino
+++ lab6/lab6.ino
@@ -1,8 +1,8 @@
 #include <Pololu3piPlus32U4.h>
 #include "printOLED.h"
 #include "PIDcontroller.h"
-//#include "odometry.h" //If using odometry, import odometry.h and odometry.cpp
-//#include "PDcontroller.h" //Import your PDcontroller.h and PDcontroller.cpp then uncomment
+#include "odometry.h" //If using odometry, import odometry.h and odometry.cpp
+#include "PDcontroller.h" //Import your PDcontroller.h and PDcontroller.cpp then uncomment
 using namespace Pololu3piPlus32U4;
 
 Motors motors;
@@ -18,31 +18,31 @@ Encoders encoders;
 #define DEAD_RECKONING false
 
 //Update kp, kd, and ki based on your testing
-#define minOutput -100
-#define maxOutput 100
-#define kp 0 //Tune Kp here
-#define kd 0 //Tune Kd here
-#define ki 0 //Tune Ki here
-#define clamp_i 0 //Tune ki integral clamp here
+#define minOutput -150
+#define maxOutput 150
+#define kp 100 //Tune Kp here
+#define kd 20 //Tune Kd here
+#define ki 2 //Tune Ki here
+#define clamp_i 15 //Tune ki integral clamp here
 #define base_speed 50
 
-//Odometry odometry(diaL, diaR, w, nL, nR, gearRatio, DEAD_RECKONING); //Uncomment if using odometry class
-//PDcontroller pdcontroller(kp, kd, minOutput, maxOutput); //Uncomment when using PDController
-//PIDcontroller pidcontroller(kp, ki, kd, minOutput, maxOutput, clamp_i); //Uncomment after you implement PIDController
+Odometry odometry(diaL, diaR, w, nL, nR, gearRatio, DEAD_RECKONING); //Uncomment if using odometry class
+PDcontroller pdcontroller(kp, kd, minOutput, maxOutput); //Uncomment when using PDController
+PIDcontroller pidcontroller(kp, ki, kd, minOutput, maxOutput, clamp_i); //Uncomment after you implement PIDController
 
 //Feel free to use this in your PD/PID controller for target values
 // Given goals in cm and radians
-const float goal_theta = 0; // Must put in radians
+const float goal_theta = -1.57; // Must put in radians
 
 //odometry
-//int16_t deltaL=0, deltaR=0;
-//int16_t encCountsLeft = 0, encCountsRight = 0;
-//float x, y, theta;
+int16_t deltaL=0, deltaR=0;
+int16_t encCountsLeft = 0, encCountsRight = 0;
+float x=0, y=0, theta=0;
 
 //controller 
 //Lab 6
 //Note: Here are some suggested variables to use for your code.
-//double PDout, PIDout; //Output variables for your controllers
+double PDout=0, PIDout=0; //Output variables for your controllers
 
 void setup() {
   Serial.begin(9600);
@@ -53,14 +53,14 @@ void loop() {
   //Use this code if you are using odometry. Comment out if you are not.
   //If using, consider turning this into its own function for repeated use.
   // Read data from encoders
-  //deltaL = encoders.getCountsAndResetLeft();
-  //deltaR = encoders.getCountsAndResetRight();
+  deltaL = encoders.getCountsAndResetLeft();
+  deltaR = encoders.getCountsAndResetRight();
 
   // Increment total encoder cound
-  //encCountsLeft += deltaL;
-  //encCountsRight += deltaR;  
+  encCountsLeft += deltaL;
+  encCountsRight += deltaR;  
 
-  //odometry.update_odom(encCountsLeft,encCountsRight, x, y, theta); //calculate robot's position
+  odometry.update_odom(encCountsLeft,encCountsRight, x, y, theta); //calculate robot's position
 
 
 
@@ -76,13 +76,32 @@ void loop() {
   /*TASK 2.2
   Utilize your PDController to go to angles PI, PI/2, and PI/2.
   Write your code below and comment out when moving to next task.*/
+  /*
+  PDout = pdcontroller.update(theta, goal_theta);
+  Serial.println(PDout);
+  Serial.println(theta);
 
+
+
+  int leftSpeed = -PDout;
+  int rightSpeed = PDout;
+  motors.setSpeeds(leftSpeed, rightSpeed);
+  */
+
+  
   /*TASK 3.1
   Implement PID controller to use for task 3.2.*/
 
   /*TASK 3.2
   Utilize your PIDController to go to angles PI, PI/2, and PI/2.
   Write your code below.*/
+  
+  PIDout = pidcontroller.update(theta, goal_theta);
+  Serial.print(theta);
 
+  int leftSpeed = -PIDout;
+  int rightSpeed = PIDout;
+  motors.setSpeeds(leftSpeed, rightSpeed);
 
 }
+
