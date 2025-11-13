7c0bd5c38870a3252baa09d1ca8e567efc1fa408
ktownsend756 <161529075+ktownsend756@users.noreply.github.com>
2025-11-13 09:58:41 +0900

    Add files via upload

---
+++ CS556_Lab11/lab11/lab11.ino
@@ -0,0 +1,219 @@
+//Starter Code
+
+#include <Pololu3piPlus32U4.h>
+#include <Pololu3piPlus32U4Buttons.h>
+using namespace Pololu3piPlus32U4;
+#include "particle_filter.h"
+#include "odometry.h"
+#include "Map.h"
+#include "sonar.h"
+#include "PIDcontroller.h"
+
+
+Map worldMap; // one map for the program
+
+ButtonA buttonA;
+ButtonB buttonB;
+ButtonC buttonC;
+
+#define PI 3.14159
+
+#define lenOfMap 60 //60 cm / 3 = 20 cm per grid unit (matches the map).
+#define N_particles 25
+#define move_noise   0.01   // σ_d^2
+#define rotate_noise 0.05   // σ_θ^2
+#define ultra_noise  0.10   // σ_s^2
+
+#define diaL 3.2 //define physical robot parameters
+#define diaR  3.2
+#define nL 12
+#define nR 12
+#define w 9.6
+#define gearRatio 75
+
+Motors motorsP;
+Encoders encoders;
+
+Odometry odometry(diaL, diaR, w, nL, nR, gearRatio);
+ParticleFilter particle(worldMap, lenOfMap, N_particles, move_noise, rotate_noise, ultra_noise);
+
+uint8_t iter =0;
+
+//odometry
+int16_t deltaL=0, deltaR=0;
+int16_t encCountsLeft = 0, encCountsRight = 0;
+float x = 10;
+float y = 10;
+float theta = 0;
+float x_last = 0.0;
+float y_last = 0.0;
+float theta_last = 0.0;
+
+
+//PID Movement
+//feel free to finetune gains and clampings
+#define kp 200
+#define kd 5
+#define ki 0.5
+#define clamp_i 50
+#define base_speed 100
+#define minOutput - 100
+#define maxOutput 100
+
+static inline float wrapPi(float a){ while(a <= -PI) a += 2*PI; while(a > PI) a -= 2*PI; return a; }
+static inline float angleErr(float target, float curr){ float a = target - curr; while(a <= -PI) a += 2*PI; while(a > PI) a -= 2*PI; return a; }
+
+
+
+//Comment out if using motion primitives
+PIDcontroller PIDcontroller(kp, ki, kd, minOutput, maxOutput, clamp_i);
+double PID_OUT_ANGLE, PID_OUT_DISTANCE;
+
+void setup() {
+    Serial.begin(9600);
+    while (!Serial) continue;
+    delay(1000);
+
+  //seed RNG for diverse particles.  // seed once
+  randomSeed(analogRead(A0));  
+ 
+  // //This is an example of how to estimate the distance to a wall for the given
+  // //map, assuming the robot is at (0, 0) and has heading PI
+  // (Optional quick test; use the global 'map')
+  float origin[2] = {0.5, 0.5};
+  float theta = 0;
+  float closestDist = worldMap.closest_distance(origin, theta);
+  Serial.println(closestDist);
+}
+
+void loop() {
+  movement();
+
+
+  //Get odometer readings  
+  deltaL = encoders.getCountsAndResetLeft();
+  deltaR = encoders.getCountsAndResetRight();
+  encCountsLeft += deltaL;
+  encCountsRight += deltaR;   
+  odometry.update_odom(encCountsLeft,encCountsRight, x, y, theta);
+
+
+  //movement function
+  //Propagate particles by using move_particles.
+  //Parameters are change from current and past odometer values
+  //Use qrapPi function to Wrap dθ when propagating particles
+  //TODO: Put code under here 
+  float c = cos(theta_last), s = sin(theta_last);
+  float dx_body = c*(x-x_last) + s*(y - y_last);
+  float dy_body = -s*(x - x_last) + c*(y - y_last);
+
+/*
+  float dx = x - x_last;
+  float dy = y - y_last;
+  float dtheta = wrapPi(theta - theta_last);
+*/
+  particle.move_particles(dx_body, dy_body, wrapPi(theta - theta_last));
+
+
+
+  //Measaure, estimation, and resample
+  //Calculate particle's posterior probabilities, calculate estimated robot's position, and resample
+  //TODO: Put code under here 
+  particle.measure();
+  particle.estimate_position();
+
+
+  // Display all particle locations and estimated robot location on screen   
+  //TODO: Put code under here 
+  particle.print_particles();
+  
+    
+  //save last odometer reading
+  //TODO: Fill in "..."
+  x_last = x;
+  y_last = y;
+  theta_last = theta;
+    
+  iter++;
+    
+  delay(1000); //for easier viewing of output
+ 
+}
+
+//DONE
+void movement(){ 
+  // Remote-Controlling 
+  int leftSpeed = 0, rightSpeed = 0;
+  float goal_theta; //Goal theta orientation
+  float goal_x, goal_y;
+  double dist_err; //Goal position (x,y)
+  if(buttonA.isPressed()){ //turn left
+    //Note, if you use PIDconrollers,know that PID assumes: output = Kp*(setpoint - measured) + ...
+    // Using a relative setpoint makes each button press add/subtract 90°
+    //your PID call needs a target angle (setpoint), not just “turn right.”
+    //PIDcontroller.update(theta, goal_theta) sets a fixed global setpoint of +π/2 (≈ 90°). That means:
+    //First left turn from θ≈0 → good (aims for +90°).
+    //Next left turn from θ≈+90° → does nothing, because you’re already at the setpoint (+90°). It won’t add another 90°.
+    //So, basically, your left turn target = current heading + 90°
+
+    //movement function here
+    //TODO: Put code under here (DONE)
+    goal_theta = wrapPi(theta + 3.14/2.0); //set goal_theta from current position for 90 degree left turn
+
+    PID_OUT_ANGLE = PIDcontroller.update(0, angleErr(goal_theta, theta)); 
+
+    leftSpeed = PID_OUT_ANGLE*.65;
+    rightSpeed = -PID_OUT_ANGLE*.65;
+    motorsP.setSpeeds(leftSpeed, rightSpeed);
+
+    if(fabs(wrapPi(goal_theta - theta)) < 0.017){ //Halt robot if it's within 1 degree of goal theta
+        motorsP.setSpeeds(0, 0);
+    }
+    while(buttonA.isPressed()){delay(5);}
+    Serial.print("Left pressed!\n");
+  } else if(buttonB.isPressed()){ // drive forward
+    //movement function here
+    //TODO: Put code under here (DONE)
+    
+    //Set the goal to be 20cm forward from current position
+    goal_x = x + 20 * cos(theta);
+    goal_y = y + 20 * sin(theta);
+
+    dist_err = sqrt(pow(goal_x - x, 2) + pow(goal_y - y, 2));
+
+    PID_OUT_DISTANCE = PIDcontroller.update(0, dist_err);
+ 
+    leftSpeed = rightSpeed = PID_OUT_DISTANCE*1.2;
+    
+    motorsP.setSpeeds(-leftSpeed, -rightSpeed);
+
+    if(fabs(dist_err) < 0.5){ //Halt robot once it is within half a centimeter from goal position
+        motorsP.setSpeeds(0, 0);
+    }
+    while(buttonB.isPressed()){delay(5);}
+
+    Serial.print("Forward pressed!\n");
+  } else if(buttonC.isPressed()){ // turn right
+    //movement function here
+    //TODO: Put code under here (DONE)
+    goal_theta = wrapPi(theta - 3.14/2.0); //set goal_theta from current position for 90 degree right turn
+
+    PID_OUT_ANGLE = PIDcontroller.update(0, angleErr(goal_theta, theta)); 
+
+    leftSpeed = PID_OUT_ANGLE*0.65;
+    rightSpeed = -PID_OUT_ANGLE*0.65;
+    motorsP.setSpeeds(leftSpeed, rightSpeed);
+
+    if(fabs(wrapPi(goal_theta - theta)) < 0.017){ //Halt robot if it's within 1 degree of goal theta
+        motorsP.setSpeeds(0, 0);
+    }
+    while(buttonC.isPressed()){delay(5);}
+
+
+    Serial.print("Right pressed!\n");
+  }
+  else{
+    motorsP.setSpeeds(0,0);
+  }
+ 
+}
