9080816c6f3da0ce0c13b504cce976c0d3f8e0eb
ryanrenales <161529467+ryanrenales@users.noreply.github.com>
2025-11-24 21:46:54 -0800

    Implement wall detection using sonar and servo
    Added servo control and sonar distance sensing for wall detection.

--- final/final.ino
+++ final/final.ino
@@ -1,14 +1,18 @@
 #include <Pololu3piPlus32U4.h>
+#include <Servo.h>
 using namespace Pololu3piPlus32U4;
 #include "odometry.h"
 #include "sonar.h"
 #include "PIDcontroller.h"
+
 
 LineSensors lineSensors;
 Motors motors;
 Servo servo;
 
 Sonar sonar = sonar(4);
+
+#define PI 3.14159
 
 //Physical Robot Parameters 
 #define diaL 3.2 
@@ -41,14 +45,93 @@ float theta_last = 0.0;
 #define maxOutput 100
 
 PIDcontroller PIDcontroller(kp, ki, kd, minOutput, maxOutput, clamp_i);
-double PID_OUT_DISTANCE;
+double PID_OUT_ANGLE, PID_OUT_DISTANCE;
+
+const float wallDist = 10.0;
+float frontDist;
+float leftDist;
+
+static inline float wrapPi(float a){ while(a <= -PI) a += 2*PI; while(a > PI) a -= 2*PI; return a; }
+static inline float angleErr(float target, float curr){ float a = target - curr; while(a <= -PI) a += 2*PI; while(a > PI) a -= 2*PI; return a; }
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  servo.attach(5);
+  servo.write(90);
+  delay(1000);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  sensing_and_movement();
+
+  //Get odometer readings  
+  deltaL = encoders.getCountsAndResetLeft();
+  deltaR = encoders.getCountsAndResetRight();
+  encCountsLeft += deltaL;
+  encCountsRight += deltaR;   
+  odometry.update_odom(encCountsLeft,encCountsRight, x, y, theta);
+
 
 }
+
+
+void sensing_and_movement(){
+  frontDist = sonar.readDist();
+  delay(500);
+  servo.write(0);
+  delay(500);
+  leftDist = sonar.readDist();
+  servo.write(90);
+  delay(500);
+
+  // case 1: Left wall detected, front wall not detected
+  if(leftDist < wallDist && frontDist > wallDist){
+    float goal_x = x + 10 * cos(theta);
+    float goal_y = y + 10 * sin(theta);
+
+    dist_err = sqrt(pow(goal_x - x, 2) + pow(goal_y - y, 2));
+    PID_OUT_DISTANCE = PIDcontroller.update(0, dist_err);
+    motors.setSpeeds(-PID_OUT_DISTANCE, -PID_OUT_DISTANCE);
+    delay(1500);
+    motors.setSpeeds(0,0);
+  }
+
+  // case 2: Left wall detected, front wall detected
+  else if(leftDist < wallDist && frontDist < wallDist){
+    float goal_theta = wrapPi(theta - PI/2.0);
+    PID_OUT_ANGLE = PIDcontroller.update(0, angleErr(goal_theta,theta));
+    motors.setSpeeds(PID_OUT_ANGLE, -PID_OUT_ANGLE);
+    delay(1500);
+    motors.setSpeeds(0,0);
+    frontDist = sonar.readDist();
+    if(frontDist < wallDist){
+      motors.setSpeeds(PID_OUT_ANGLE, -PID_OUT_ANGLE);
+      delay(1500);
+      motors.setSpeeds(0,0);
+      delay(500);
+    }
+  }
+
+  // case 3: Left wall not detected, front wall detected OR no wall detected
+  else{
+    float goal_theta = wrapPi(theta - PI/2.0);
+    PID_OUT_ANGLE = PIDcontroller.update(0, angleErr(goal_theta,theta));
+    motors.setSpeeds(-PID_OUT_ANGLE, PID_OUT_ANGLE);
+    delay(1500);
+    motors.setSpeeds(0,0);
+    
+    float goal_x = x + 10 * cos(theta);
+    float goal_y = y + 10 * sin(theta);
+
+    dist_err = sqrt(pow(goal_x - x, 2) + pow(goal_y - y, 2));
+    PID_OUT_DISTANCE = PIDcontroller.update(0, dist_err);
+    motors.setSpeeds(-PID_OUT_DISTANCE, -PID_OUT_DISTANCE);
+    delay(1500);
+    motors.setSpeeds(0,0);
+  }
+
+  
+}
+
